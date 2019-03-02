f3ec39de9614ed3d7b51a3657698641f3c5de741
Keith Walker <Keithrwalker418@gmail.com>
2019-03-02 11:43:27 -0600

    added dogBreed function for challenege

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const nuggetMaker = (animal) => {
     return `${animal} Nuggets`;
 };
 
+const dogBreed = (breed) => {
+    return `My favorite dog breed is ${breed}`;
+};
+
 namePrint('Keith', 'Walker');
 
 console.log(nuggetMaker('Chicken'));
+
+console.log(dogBreed('Border Collie'));
